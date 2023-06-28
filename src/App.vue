a87da86fd7149efcde4966202ec50b56b6cd5544
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-06-28 17:04:01 +0200

    appTitle fixed

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,20 @@
 export default {
   data() {
     return {
-
+      phrase: 'La mia prima app con Vite!'
     }
   }
 }
 </script>
 
 <template>
-  <h1>Hello world!</h1>
+  <h1>{{ phrase }}</h1>
 </template>
 
-<style></style>
+<style>
+h1 {
+  color: blue;
+  text-shadow: 2px 2px red;
+  text-align: center;
+}
+</style>
